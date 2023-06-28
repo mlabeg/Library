b50f97116236ad5bbdc93114ee5124d5c50504b3
Mateusz Gębal <s10230mg@ms.wwsi.edu.pl>
2023-06-28 23:33:14 +0200

    zliczanie książek w zamówieniu, poprawa wyświetlania menu

---
+++ Presentation/Library.ConsoleApp/TODOs.cs
@@ -0,0 +1,8 @@
+﻿//TODO 2.51 sprawdzić cały projekt przy pomocy CodeMaid
+
+
+//TODO 7 możesz pokombinować z PadRight(), ale to w sumie nie takie ważne @ListCurrentOrder @140
+//TODO 7 ostatnia linia ma więcej białych znaków jeśli nazwa książki jest krótsza
+
+
+//3 - przed publikacją na github
